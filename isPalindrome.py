08d974e9287a6550065fdc853f41a5c2a4a8a0c5
Daron Weekley <dweekley99@gmail.com>
2024-08-27 10:57:58 -0400

    updated isPalindrome class and main.py with comments and answered question 1

--- isPalindrome.py
+++ isPalindrome.py
@@ -1,29 +1,35 @@
 class isPalindrome:
     def palindrome_check(str, start_length, end_length):
-        if start_length == end_length:
+        '''Recursive function that takes the parameters of the string, starting position of the str(start_length)
+        and ending position of the string(end_length) to determine if the string given is a palindrome and returns True or False'''
+        
+        if start_length == end_length: # if start and end length are the same, the string is a palindrome
             return True
         
-        if str[start_length] != str[end_length]:
-            return False
+        if str[start_length] != str[end_length]: # compares the start length to the end length of a string 
+            return False # returns false if the character is not the same
         
-        if start_length < end_length + 1:
-            return isPalindrome.palindrome_check(str, start_length + 1, end_length - 1)
+        if start_length < end_length + 1: # cycles through the function as long as the start length is less than the end length
+            return isPalindrome.palindrome_check(str, start_length + 1, end_length - 1) # increases the start length and decrease the end length
         
-        return True
+        return True # returns true when the start length is greater than the end length
     
     def initialize_pal(self, str):
-        sl = 0
-        el = len(str) - 1 
+        '''This function initializes the string to be checked by determining the ending length of the string
+        and calling the palindrome check function as long as the string is not 1 character long'''
+        sl = 0 # beginning position
+        el = len(str) - 1 # length of string minus 1 to get the overall end position
 
-        if sl == el:
+        if sl == el: # determines if the starting length and ending length are the same
             return True
         
-        else:
-            return isPalindrome.palindrome_check(str, sl, el)
+        else: # uses palindrome_check with the string, starting length, and ending length initialized
+            return self.palindrome_check(str, sl, el)
     
     def print_statement(self, text):
-        b = self.initialize_pal(text)
-        if b == True:
+        '''returns a statement to tell the user if the string the entered is a palindrome'''
+        b = self.initialize_pal(text) # initializes the string to check if it is a palindrome
+        if b == True: # if initialize_pal is True then it is a palindrome
             return f'{text} is a palindrome.'
-        else:
+        else: # if not True it is not a palindrome
             return f'{text} is not a palindrome.'
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 from isPalindrome import isPalindrome
 
-ip = isPalindrome()
-run = True
-while run:
-    text = input("Enter a string to check for palindrome. Q to quit: ").lower()
-    if text == "q":
+ip = isPalindrome() # initializes class isPalindrome for future use
+run = True 
+while run: # allows the program to cycle through until the user inputs "q"
+    text = input("Enter a string to check for palindrome. Q to quit: ").lower() # takes the users input and converts it to lower case
+    if text == "q": # if "q" is entered by the user the program terminates
         print("Goodbye")
         run = False 
-    else:
+    else: # all other strings will use the print_statement function to determine if the string is a palindrome
         result = ip.print_statement(text)
         print(result)
 
